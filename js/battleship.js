1a05321039b2d9303dea928373dc05da0b45992d
GabeValdivia <gabe@gabeswebcoding.com>
2021-10-12 11:37:15 -0500

    Add view sudo code

---
+++ js/battleship.js
@@ -0,0 +1,12 @@
+var view = {
+    //this method takes a string message and displays it in the message area
+    displayMessage: function (msg) {
+        //code to be supplied in  bit!
+    }
+    displayHit: function(location){
+    	//code will go here
+    },
+    displayMiss: function(location){
+        //code will go here
+    }
+};
